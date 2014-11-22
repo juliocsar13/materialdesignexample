57d6ee6b0dc02030e28bd1c70ca5c97a0da0ca0a
Carlos Bedoy <carlos.cervantes@pademobile.com>
2014-11-21 18:48:47 -0600

    Added support by unit test

--- app/src/androidTest/java/cbedoy/materialdesignexample/ApplicationTest.java
+++ app/src/androidTest/java/cbedoy/materialdesignexample/ApplicationTest.java
@@ -3,6 +3,9 @@ package cbedoy.materialdesignexample;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
