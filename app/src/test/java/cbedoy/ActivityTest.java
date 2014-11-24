b30e2917b588a20763df696327fe150bbf0cd6ce
Carlos Bedoy <carlos.cervantes@pademobile.com>
2014-11-24 14:25:32 -0600

    Change name by acitivies

--- app/src/test/java/cbedoy/ActivityTest.java
+++ app/src/test/java/cbedoy/ActivityTest.java
@@ -13,17 +13,13 @@ import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
-import cbedoy.materialdesignexample.FuckinActivity;
-import cbedoy.materialdesignexample.MainViewController;
+import cbedoy.materialdesignexample.RoboelectricActivity;
 import cbedoy.materialdesignexample.R;
 
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
 
 
-/**
- * Created by vandekr on 11/02/14.
- */
 @Config(emulateSdk = 18)
 @RunWith(RobolectricTestRunner.class)
 public class ActivityTest {
@@ -34,7 +30,7 @@ public class ActivityTest {
 
     @Test
     public void testActivityFound() {
-        Activity activity = Robolectric.buildActivity(FuckinActivity.class).create().get();
+        Activity activity = Robolectric.buildActivity(RoboelectricActivity.class).create().get();
 
         Button pressMeButton = (Button) activity.findViewById(R.id.press_me_button);
         TextView results = (TextView) activity.findViewById(R.id.results_text_view);
